9562deb81091125eace60612945cc0d03bd70c9c
Mikhiel <mikhielbenji9@gmail.com>
2024-06-20 18:51:52 +0530

    Added try-catch for errors

--- app.py
+++ app.py
@@ -9,31 +9,32 @@ CORS(app)  # This will enable CORS for all routes
 # Load the model
 model = pickle.load(open("model.pkl", "rb"))
 
-
 @app.route("/")
 def welcome():
     return "You Are Welcome to COVID-19 Detector."
 
-
 @app.route("/predict", methods=["POST"])
 def predict():
-    # Get the data from the POST request
-    int_features = [int(x) for x in request.json.values()]
-    features = [np.array(int_features)]
+    try:
+        # Get the data from the POST request
+        int_features = [int(x) for x in request.json.values()]
+        features = [np.array(int_features)]
 
-    # Make a prediction
-    prediction = model.predict(features)[0]
+        # Make a prediction
+        prediction = model.predict(features)[0]
 
-    # Prepare the response
-    if (prediction == 1):
-        response = {
-            "prediction_text": "You are a Covid Patient, Kindly go to your nearby Hospital"}
-    else:
-        response = {
-            "prediction_text": "You are not a Covid Patient, but kindly go out for necessary reasons."}
-
-    return jsonify(response)
-
+        # Prepare the response
+        if prediction == 1:
+            response = {
+                "prediction_text": "You are a Covid Patient, Kindly go to your nearby Hospital"}
+        else:
+            response = {
+                "prediction_text": "You are not a Covid Patient, but kindly go out for necessary reasons."}
+        
+        return jsonify(response)
+    
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run(debug=True)
